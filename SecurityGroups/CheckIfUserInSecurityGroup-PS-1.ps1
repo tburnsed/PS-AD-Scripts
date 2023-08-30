9545dad33ebf0d09418c93ad34beac742b6ffd5d
Timothy Burnsed <143354694+tburnsed@users.noreply.github.com>
2023-08-30 11:49:19 -0400

    Secuirty Group Script Orginal upload

---
+++ SecurityGroups/CheckIfUserInSecurityGroup-PS-1.ps1
@@ -0,0 +1,12 @@
+﻿$users = Import-CSV "FILEPATH" | select -expandproperty users
+
+foreach ($user in $users) {
+    $secgroup = 'NAME-OF-SECUIRTY-GROUP'
+    $members = Get-ADGroupMember -Identity $secgroup -Recursive | Select -ExpandProperty name
+
+    If ($members -contains $user) {
+        Write-Host $user is a member of $secgroup -ForegroundColor green
+    } Else {
+        Write-Host $user is not a member of $secgroup -ForegroudColor red
+    }
+}
